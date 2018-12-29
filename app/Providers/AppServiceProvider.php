c0354b50b75c24a68821ed627ca97d65e5be7af3
kallbuloso <kallbuloso@gmail.com>
2018-12-29 20:50:59 -0200

    Alterar os models para o diretório "Models"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        \Carbon\Carbon::setLocale(str_replace('_', '-', app()->getLocale()));
         //
+        
     }
 
     /**
@@ -23,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton(\Faker\Generator::class, function () {
+            return \Faker\Factory::create(str_replace('_', '-', app()->getLocale()));
+            // return \Faker\Factory::create(app()->getLocale());
+        });
         //
     }
 }
