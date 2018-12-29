c21d6341720cc35f2e6cbf5860a7694fbffe0b3a
kallbuloso <kallbuloso@gmail.com>
2018-12-29 21:44:26 -0200

    Adicionado Directivas Blade Customizadas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Form;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Providers\Blade\DirectivesRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +18,12 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         \Carbon\Carbon::setLocale(str_replace('_', '-', app()->getLocale()));
+        $this->registerDirectives();
+        $this->registerCustonsDirectives();
+        // Group Components
+        // Form::component('textGroup','components.formGroup.text', ['col', 'label', 'name', 'value' => null, 'attributes' => [], 'errors']);
+        Form::component('inputGroup','components.formGroup.input', ['type', 'params', 'errors']);
+        Form::component('elementGroup','components.formGroup.element', ['type', 'params', 'errors']);
         //
         
     }
@@ -32,5 +40,28 @@ class AppServiceProvider extends ServiceProvider
             // return \Faker\Factory::create(app()->getLocale());
         });
         //
+    }
+
+    
+    /**
+     * Register all directives.
+     *
+     * @return void
+     */
+    public function registerDirectives()
+    {
+        $directives = require __DIR__.'/Blade/BladeDirectives.php';
+        DirectivesRepository::register($directives);
+    }
+
+    /**
+     * Register Custons Directives
+     * 
+     * @return void
+     */
+    public function registerCustonsDirectives()
+    {
+        $directives = require __DIR__.'/Blade/BladeCustonsDirectives.php';
+        DirectivesRepository::register($directives);
     }
 }
